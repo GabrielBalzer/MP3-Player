6d958ad9ae0d0f12feaedaaf35847a4b058124b6
Gabriel Balzer <gbalzer96@gmail.com>
2020-10-20 13:56:29 +0200

    Add project files.

---
+++ .shared/SharedAssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("GabrielBalzer")]
+[assembly: AssemblyProduct("Ui.Desktop")]
+[assembly: AssemblyCopyright("Copyright © GabrielBalzer 2020")]
+[assembly: AssemblyTrademark("GabrielBalzer")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
