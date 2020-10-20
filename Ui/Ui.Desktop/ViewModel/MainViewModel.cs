c9a6943f097fd4bbbe67cd780116620591325224
Gabriel Balzer <gbalzer96@gmail.com>
2020-10-20 14:18:42 +0200

    Add DataContex Binding

--- Ui/Ui.Desktop/ViewModel/MainViewModel.cs
+++ Ui/Ui.Desktop/ViewModel/MainViewModel.cs
@@ -2,18 +2,6 @@ using GalaSoft.MvvmLight;
 
 namespace MP3Player.Ui.Desktop.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
         /// <summary>
@@ -21,14 +9,16 @@ namespace MP3Player.Ui.Desktop.ViewModel
         /// </summary>
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            if (IsInDesignMode)
+            {
+                WindowTitle = "Design";
+            }
+            else
+            {
+                WindowTitle = "MP3Player";
+            }
         }
+
+        public string WindowTitle { get; private set; }
     }
 }
