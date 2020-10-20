2aa57a8834d448a5430e6c47b50dadecc7e505d3
Gabriel Balzer <gbalzer96@gmail.com>
2020-10-20 16:08:32 +0200

    AudioTest

--- Logic/Logic.Ui/MainViewModel.cs
+++ Logic/Logic.Ui/MainViewModel.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 using GalaSoft.MvvmLight;
+using Microsoft.Win32;
 
 namespace MP3Player.Logic.Ui
 {
@@ -28,11 +30,21 @@ namespace MP3Player.Logic.Ui
 
         public ICommand ButtonCommand { get; set; }
 
+        public MediaPlayer mediaPlayer = new MediaPlayer();
+
 
         private void MainButtonClick(object sender)
         {
-            throw new NotImplementedException();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mediaPlayer.Open(new Uri(openFileDialog.FileName));
+                mediaPlayer.Play();
+            }
         }
+
+        
 
     }
 }
