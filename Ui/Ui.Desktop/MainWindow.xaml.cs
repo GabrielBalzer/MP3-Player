4e2c17c6f889f67ff52f225793ad6f29e6e36903
Gabriel Balzer <gbalzer96@gmail.com>
2020-10-20 14:45:34 +0200

    Add Mahapps.Retro

--- Ui/Ui.Desktop/MainWindow.xaml.cs
+++ Ui/Ui.Desktop/MainWindow.xaml.cs
@@ -1,15 +1,22 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using MahApps.Metro.Controls;
 
 namespace MP3Player.Ui.Desktop
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LaunchGitHubSite(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
