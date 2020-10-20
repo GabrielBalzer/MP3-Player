400c2f2ec36474f4dc94214aa64f08bd8a139e58
Gabriel Balzer <gbalzer96@gmail.com>
2020-10-20 17:44:18 +0200

    Add Play/Pause/Stop Buttons

--- Logic/Logic.Ui/MainViewModel.cs
+++ Logic/Logic.Ui/MainViewModel.cs
@@ -24,11 +24,22 @@ namespace MP3Player.Logic.Ui
             }
 
             ButtonCommand = new RelayCommand(o => MainButtonClick("MainButton"));
+            PlayButton = new RelayCommand(o => PlayButtonClick("PlayButton"));
+            PauseButton = new RelayCommand(o => PauseButtonClick("PauseButton"));
+            StopButton = new RelayCommand(o => StopButtonClick("StopButton"));
         }
+
+
 
         public string WindowTitle { get; private set; }
 
         public ICommand ButtonCommand { get; set; }
+
+        public ICommand PlayButton { get; set; }
+
+        public ICommand PauseButton { get; set; }
+
+        public ICommand StopButton { get; set; }
 
         public MediaPlayer mediaPlayer = new MediaPlayer();
 
@@ -40,11 +51,27 @@ namespace MP3Player.Logic.Ui
             if (openFileDialog.ShowDialog() == true)
             {
                 mediaPlayer.Open(new Uri(openFileDialog.FileName));
-                mediaPlayer.Play();
+                mediaPlayer.Volume = 0.1;
+
             }
         }
 
-        
+        private void PlayButtonClick(object sender)
+        {
+            mediaPlayer.Play();
+        }
+
+        private void PauseButtonClick(object sender)
+        {
+            mediaPlayer.Pause();
+        }
+
+        private void StopButtonClick(object sender)
+        {
+            mediaPlayer.Stop();
+        }
+
+
 
     }
 }
