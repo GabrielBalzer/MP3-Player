a234ae5b71dc2d559630c6b7eb7fc1bb2fa448a7
Gabriel Balzer <gbalzer96@gmail.com>
2021-05-21 14:14:12 +0200

    Add PlayListHandlerInterface

---
+++ Logic/Logic.Ui/IPlaylistHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace MP3Player.Logic.Ui
+{
+    public interface IPlaylistHandler
+    {
+        TrulyObservableCollection<SingleTrack> returnWholePlaylist();
+        void addSongToPlaylist(SingleTrack track);
+        SingleTrack returnFirstTrack();
+        int getNumberOfTracks();
+        SingleTrack GetNextTrack();
+        SingleTrack GetLastTrack();
+    }
+}
