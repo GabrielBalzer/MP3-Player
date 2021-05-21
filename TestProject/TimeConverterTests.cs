01d3c811884810181187585d91a0d94b82be8fcb
Gabriel Balzer <gbalzer96@gmail.com>
2021-05-21 15:00:08 +0200

    UnitTest #7

---
+++ TestProject/TimeConverterTests.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using MP3Player.Logic.Ui;
+using Xunit;
+
+namespace TestProject
+{
+    public class TimeConverterTests
+    {
+        [Fact]
+        public void TestGetCurrentTrackTimeAsString()
+        {
+            var expectedTime = "02 : 15";
+
+            var songPlayerMock = new Mock<ISongPlayer>();
+            songPlayerMock.Setup(p => p.GetCurrentTrackTimeInSeconds()).Returns(135);
+
+            Assert.Equal(expectedTime, TimeConverter.GetCurrentTrackTimeAsString(songPlayerMock.Object));
+        }
+    }
+}
