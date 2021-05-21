a20cbb360a7326384362660eaf6577c9d3914438
Gabriel Balzer <gbalzer96@gmail.com>
2021-05-21 14:18:46 +0200

    UnitTest#6 Mock

--- TestProject/SongPlayerTests.cs
+++ TestProject/SongPlayerTests.cs
@@ -70,6 +70,25 @@ namespace TestProject
             Assert.Equal(expectedHighValue, songPlayer.GetCurrentVolume(), 2);
         }
 
+        [Fact]
+        public void CheckIfSongStartsPlayingIfNoTrackInPlaylist()
+        {
 
+            string expectedResult = "No Track in Playlist";
+
+            var playListHandlerMock = new Mock<IPlaylistHandler>();
+            playListHandlerMock.Setup(p => p.returnFirstTrack()).Returns((SingleTrack)null);
+
+            SongPlayer songPlayer = new SongPlayer(playListHandlerMock.Object);
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                songPlayer.PlaySong();
+
+                var result = sw.ToString().Trim();
+                Assert.Equal(expectedResult, result);
+            }
+        }
     }
 }
