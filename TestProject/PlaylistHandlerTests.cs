6f065679441bea24bdfef3534edb8ba03569a669
Gabriel Balzer <gbalzer96@gmail.com>
2021-05-21 15:47:20 +0200

    UnitTest #9

--- TestProject/PlaylistHandlerTests.cs
+++ TestProject/PlaylistHandlerTests.cs
@@ -5,6 +5,21 @@ namespace TestProject
 {
     public class PlaylistHandlerTests
     {
+        [Fact]
+        public void TestIfFirstElementIsReturnedCorrectly()
+        {
+            PlaylistHandler playlistHandler = new PlaylistHandler();
+            var expecteSingleTrack = new SingleTrack("test1", "test1", "1", true);
 
+            playlistHandler.addSongToPlaylist(new SingleTrack("test1", "test1", "1", true));
+            playlistHandler.addSongToPlaylist(new SingleTrack("test2", "test2", "2", false));
+
+            var resultSingleTrack = playlistHandler.returnFirstTrack();
+
+            Assert.Equal(expecteSingleTrack.FilePath, resultSingleTrack.FilePath);
+            Assert.Equal(expecteSingleTrack.TrackName, resultSingleTrack.TrackName);
+            Assert.Equal(expecteSingleTrack.TrackNumber, resultSingleTrack.TrackNumber);
+            Assert.Equal(expecteSingleTrack.IsPlaying, resultSingleTrack.IsPlaying);
+        }
     }
 }
