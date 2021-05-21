3c3386663875f46572e6e046baf0c58115c3bf02
Gabriel Balzer <gbalzer96@gmail.com>
2021-05-21 08:44:10 +0200

    Unit Test #1

---
+++ TestProject/SongPlayerTests.cs
@@ -0,0 +1,21 @@
+using System;
+using MP3Player.Logic.Ui;
+using Xunit;
+
+namespace TestProject
+{
+    public class SongPlayerTests
+    {
+        [Fact]
+        public void PlayerStartsInPauseState()
+        {
+            bool expectedPauseStatus = true;
+
+            SongPlayer songPlayer = new SongPlayer();
+
+            Assert.Equal(expectedPauseStatus, songPlayer.getPauseStatus());
+        }
+
+
+    }
+}
