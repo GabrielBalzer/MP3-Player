b6054bc79892c687f15933baf46ad95ac7c16506
Gabriel Balzer <gbalzer96@gmail.com>
2021-05-21 14:14:23 +0200

    Add SongPlayerInterface

---
+++ Logic/Logic.Ui/ISongPlayer.cs
@@ -0,0 +1,23 @@
+﻿using NAudio.Wave;
+
+namespace MP3Player.Logic.Ui
+{
+    public interface ISongPlayer
+    {
+        void SetVolume(double volume);
+        float GetCurrentVolume();
+        bool GetPauseStatus();
+        void PlaySong();
+        void OnPlayBackStopped(object sender, StoppedEventArgs e);
+        void PauseSong();
+        void StopSong();
+        void PlayNextSong();
+        void PlayLastSong();
+        double GetTrackLengthInSeconds();
+        double GetCurrentTrackTimeInSeconds();
+        void ClearSongReader();
+        int GetCurrentTrackProgress();
+        void SetPosition(int value);
+
+    }
+}
