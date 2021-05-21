69d906a60aa7775f467efbd7c0a2e21439b9b5c9
Gabriel Balzer <gbalzer96@gmail.com>
2021-05-21 15:17:34 +0200

    Use IPlaylistHandler in FileManager

--- Logic/Logic.Ui/FileManager.cs
+++ Logic/Logic.Ui/FileManager.cs
@@ -7,12 +7,11 @@ namespace MP3Player.Logic.Ui
     public class FileManager
     {
         private readonly OpenFileDialog fileDialog = new OpenFileDialog();
-        public void AddFileToPlaylist(PlaylistHandler playlistHandler)
+        public void AddFileToPlaylist(IPlaylistHandler playlistHandler)
         {
             fileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
             if (fileDialog.ShowDialog() == true)
             {
-                //SongPlayer.PickFile(fileDialog.FileName);
                 var friendlyName = fileDialog.SafeFileName.Remove(fileDialog.SafeFileName.Length - 4);
                 var numberOfTracks = playlistHandler.getNumberOfTracks();
                 playlistHandler.addSongToPlaylist(new SingleTrack(fileDialog.FileName, friendlyName, numberOfTracks.ToString(), false));
