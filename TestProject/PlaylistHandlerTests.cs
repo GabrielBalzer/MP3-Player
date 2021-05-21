979b52e427b825c96bc824e4e3b48d5c11db8d7e
Gabriel Balzer <gbalzer96@gmail.com>
2021-05-21 15:52:00 +0200

    UnitTest #10

--- TestProject/PlaylistHandlerTests.cs
+++ TestProject/PlaylistHandlerTests.cs
@@ -21,5 +21,22 @@ namespace TestProject
             Assert.Equal(expecteSingleTrack.TrackNumber, resultSingleTrack.TrackNumber);
             Assert.Equal(expecteSingleTrack.IsPlaying, resultSingleTrack.IsPlaying);
         }
+
+        [Fact]
+        public void TestIfNextElementIsReturnedAndIsPlayingStateIsChangedCorrectly()
+        {
+            PlaylistHandler playlistHandler = new PlaylistHandler();
+            var expectedSingleTrack = new SingleTrack("testy", "testy", "2", true);
+
+            playlistHandler.addSongToPlaylist(new SingleTrack("testx", "testx", "1", true));
+            playlistHandler.addSongToPlaylist(new SingleTrack("testy", "testy", "2", false));
+
+            var resultSingleTrack = playlistHandler.GetNextTrack();
+
+            Assert.Equal(expectedSingleTrack.FilePath, resultSingleTrack.FilePath);
+            Assert.Equal(expectedSingleTrack.TrackName, resultSingleTrack.TrackName);
+            Assert.Equal(expectedSingleTrack.TrackNumber, resultSingleTrack.TrackNumber);
+            Assert.Equal(expectedSingleTrack.IsPlaying, resultSingleTrack.IsPlaying);
+        }
     }
 }
