2d25f426350ebe5d170aacf3fda0898e28c3a1a6
Gabriel Balzer <gbalzer96@gmail.com>
2021-05-23 11:23:47 +0200

    cleanup

--- TestProject/TimeConverterTests.cs
+++ TestProject/TimeConverterTests.cs
@@ -6,15 +6,6 @@ namespace TestProject
 {
     public class TimeConverterTests
     {
-        [Fact]
-        public void TestGetCurrentTrackTimeAsString()
-        {
-            var expectedTime = "02 : 15";
 
-            var songPlayerMock = new Mock<ISongPlayer>();
-            songPlayerMock.Setup(p => p.GetCurrentTrackTimeInSeconds()).Returns(135);
-
-            Assert.Equal(expectedTime, TimeConverter.GetCurrentTrackTimeAsString(songPlayerMock.Object));
-        }
     }
 }
