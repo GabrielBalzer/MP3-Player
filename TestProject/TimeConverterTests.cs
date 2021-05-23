c5cd3ab00d4d30a17bc319cf8039639db648a23d
Gabriel Balzer <gbalzer96@gmail.com>
2021-05-23 11:26:44 +0200

    Unit Test #7

--- TestProject/TimeConverterTests.cs
+++ TestProject/TimeConverterTests.cs
@@ -6,6 +6,16 @@ namespace TestProject
 {
     public class TimeConverterTests
     {
+        [Fact]
+        public void TestGetCurrentTrackTimeAsString()
+        {
+            var expectedTime = "02 : 15";
 
+            var songPlayerMock = new Mock<ISongPlayer>();
+            songPlayerMock.Setup(p => p.GetCurrentTrackTimeInSeconds()).Returns(135);
+
+            Assert.Equal(expectedTime, TimeConverter.GetCurrentTrackTimeAsString(songPlayerMock.Object));
+            songPlayerMock.Verify();
+        }
     }
 }
